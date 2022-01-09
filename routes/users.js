503bacb4721ad23f1fa12c4c4aa82ce3f78c0b04
Jeffery <parkjon8@gmail.com>
2022-01-09 20:41:44 +0000

    Set up users routes

--- routes/users.js
+++ routes/users.js
@@ -8,18 +8,42 @@
 const express = require('express');
 const router  = express.Router();
 
-module.exports = (db) => {
-  router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users;`)
-      .then(data => {
-        const users = data.rows;
-        res.json({ users });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  });
-  return router;
-};
+router.get('/', (req, res) => {
+  res.send(req);
+  // res.send('users page');
+})
+
+router.post('/', (req, res) => {
+  res.send(req.query);
+  // res.send('users page - submit order and proceed to checkout');
+})
+
+router.get('/checkout', (req, res) => {
+  res.send('checkout page');
+})
+
+router.post('/checkout', (req, res) => {
+  res.send('checkout page - chekout and proceed to confirmation page');
+})
+
+router.get('/confirmation', (req, res) => {
+  res.send('confirmation page');
+})
+
+module.exports = router;
+
+// module.exports = (db) => {
+//   router.get("/", (req, res) => {
+//     db.query(`SELECT * FROM users;`)
+//       .then(data => {
+//         const users = data.rows;
+//         res.json({ users });
+//       })
+//       .catch(err => {
+//         res
+//           .status(500)
+//           .json({ error: err.message });
+//       });
+//   });
+//   return router;
+// };
