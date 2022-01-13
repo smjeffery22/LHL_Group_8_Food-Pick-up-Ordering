02f59346693f51856503eea6f226537422c957ac
Jeffery <parkjon8@gmail.com>
2022-01-13 01:09:09 +0000

    Update menu page script file to update array cart and update route names

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,7 @@
 // Client facing scripts here
 $(document).ready(function () {
   // add items to the cart
-  const cart = [];
+  const cartArray = [];
   const addCartButtons = $.find('.add-cart-button');
 
   for (const addCartButton of addCartButtons) {
@@ -15,6 +15,16 @@ $(document).ready(function () {
       // prevent duplicate items being added to the cart
       if (isItemDuplicate) {
         cartList.append(addToCart(itemNumber, itemName, itemPrice));
+
+        cartArray.push({
+          number: itemNumber,
+          name: itemName,
+          price: itemPrice,
+          qty: 1,
+          total: 123
+        });
+        console.log(cartArray);
+
       } else {
         alert('Already added in the cart!');
       }
@@ -22,6 +32,7 @@ $(document).ready(function () {
       // remove items from the cart
       $(`#remove-cart-${itemNumber}`).on('click', function () {
         $(this).parent().remove();
+        rmvCartArrItem(cartArray, itemNumber);
       })
 
       // update qty in cart
@@ -31,33 +42,30 @@ $(document).ready(function () {
       $(`#cart-qty-add-${itemNumber}`).on('click', function () {
         cartQty += 1;
         cartQtyEl.val(cartQty);
+        updateCartArrQty(cartArray, itemNumber, cartQty);
       })
 
       $(`#cart-qty-rmv-${itemNumber}`).on('click', function () {
         if (cartQty <= 1) {
           $(this).parent().remove();
+          rmvCartArrItem(cartArray, itemNumber);
         } else {
           cartQty -= 1;
           cartQtyEl.val(cartQty);
+          updateCartArrQty(cartArray, itemNumber, cartQty);
         }
       })
     })
-
   }
   $('#checkout').on('click', () => {
-    console.log('checkout');
+    window.localStorage.setItem('cart', JSON.stringify(cartArray));
+    console.log(window.localStorage.getItem('cart'));
   })
 })
 
 
-// cart.push({
-//   number: itemNumber,
-//   name: itemName,
-//   price: itemPrice,
-//   qty: 1,
-//   total: 123
-// });
 
+// add to cart list
 const addToCart = (number, name, price) => {
   return `
   <div id="cart-${number}" class="cart-item">
@@ -69,10 +77,23 @@ const addToCart = (number, name, price) => {
   </div>`
 }
 
+// delete cart item from cart array
+const rmvCartArrItem = (cartArr, itemNumber) => {
+  for (const cartItem of cartArr) {
+    if (cartItem.number === itemNumber) {
+      const index = cartArr.indexOf(cartItem);
+      cartArr.splice(index, 1);
+      // console.log('current cart:', cartArr);
+    }
+  }
+};
 
-
-
-
-
-
-
+// update item qty in cart array
+const updateCartArrQty = (cartArr, itemNumber, cartQty) => {
+  for (const cartItem of cartArr) {
+    if (cartItem.number === itemNumber) {
+      cartItem.qty = cartQty;
+      console.log('current cart:', cartArr);
+    }
+  }
+};
