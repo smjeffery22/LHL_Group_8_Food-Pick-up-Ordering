e8f5b2cb84a64600c58f3da5c201d076bd778477
Fardosa Garad <fardosamgarad@gmail.com>
2022-01-14 00:06:49 -0500

    final touches x2

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const { sendEstimatedTimeSMS, sendOrderReadySMS } = require('../send-sms');
 const router = express.Router();
 
 const adminRoutes = (db) => {
   router.get("/", (req, res) => {
     res.render('admin');
   });
+
+  router.post("/time_notification", (req, res) => {
+    const time = req.body.timeestimate;
+    sendEstimatedTimeSMS(time);
+    res.send('Estimated time sent.')
+  })
+
+  router.post("/order_update", (req, res) => {
+    sendOrderReadySMS();
+    res.send('Order update sent.')
+  })
 
   return router;
 };
