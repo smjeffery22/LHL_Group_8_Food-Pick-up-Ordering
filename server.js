922d8c797986ada06ccb453ae429c6ba63d2064a
Jeffery <parkjon8@gmail.com>
2022-01-17 03:07:12 +0000

    Clean files for midterm submission

--- server.js
+++ server.js
@@ -67,19 +67,6 @@ app.get("/", (req, res) => {
   res.render("main");
 });
 
-app.get("/menu", (req, res) => {
-  res.render("index");
-});
-
-
-app.get("/checkout", (req, res) => {
-  res.render("checkout");
-});
-
-// app.get("/checkout", (req, res) => {
-//   res.render("checkout");
-// });
-
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
